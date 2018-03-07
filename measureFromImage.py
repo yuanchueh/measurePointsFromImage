7dfe3ee642ddd2dba43ce8ffd554a840cb5e0c00
Unknown <yuanchueh@gmail.com>
2018-03-06 18:35:07 -0600

    Auto stash before merge of "master" and "origin/master"

--- measureFromImage.py
+++ measureFromImage.py
@@ -1,5 +1,6 @@
 from math import sqrt, pow
 from PIL import Image, ImageDraw, ImageFont
+<<<<<<< Updated upstream
 # This function will be used to define the calibration value to convert between pixels and units.
 # Acceptable units are SI (um, mm, cm, m, km) and English (in, ft, mi)
 # Internally all units will be handled in mm and converted?
@@ -35,3 +36,43 @@ p1 = [1,2]
 p2 = [2,5]
 dist = calculateDistance(p1, p2, calibUnitValue)
 print('calulate distance' , dist, 'and the calibration unit is ', calibUnit, '. The conversion value is', calibUnitValue)
+=======
+from os import sys
+#from imutils import perspective
+#from __future__ import print_function
+
+# get an image
+base = Image.open("car.png").convert('RGBA')
+base# = Image.open('Pillow/Tests/images/lena.png').convert('RGBA')
+print(base.format, base.size, base.mode)
+
+#draw = ImageDraw.Draw(im)
+#draw.line((0, 0) + im.size, fill=128)
+#draw.line((0, im.size[1], im.size[0], 0), fill=128)
+#del draw
+
+#Draw lines
+line = ImageDraw.Draw.line(10,10,75,75)
+
+
+# make a blank image for the text, initialized to transparent text color
+txt = Image.new('RGBA', base.size, (255,255,255,0))
+
+# get a font
+fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
+# get a drawing context
+d = ImageDraw.Draw(txt)
+
+# draw text, half opacity
+d.text((10,10), "Hello", font=fnt, fill=(255,255,255,128))
+# draw text, full opacity
+d.text((10,60), "World", font=fnt, fill=(255,255,255,255))
+
+out = Image.alpha_composite(base, txt)
+out = Image.alpha_composite(out, line)
+
+out.show()
+
+# write to stdout
+#im.save(sys.stdout, "PNG")
+>>>>>>> Stashed changes
